7e0268c4535937e403f15858d2e7f438982f797b
Robin Da Silva <robindasilva@Robins-MacBook-Pro.local>
2018-02-14 23:08:10 +0000

    5th commit

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -17,30 +17,32 @@ accessToken = jsonResponse.access_token;      });
 },
 
 
-  search(term, location, sortBy) {
+search(term, location, sortBy) {
+  return Yelp.getAccessToken().then(() => {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
+      headers: { Authorization: `Bearer ${accessToken}` }
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
       if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => ({
-          id: business.id,
-          imageSrc: business.image_url,
-          name: business.name,
-          address: business.location.address1,
-          city: business.location.city,
-          state: business.location.state,
-          zipCode: business.location.zip_code,
-          category: business.categories[0].title,
-          rating: business.rating,
-          reviewCount: business.review_count
-        }));
+        return jsonResponse.businesses.map(business => {
+          console.log(business);
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
       }
     });
-  }
+  });
 };
 
 
